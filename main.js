26ecb42eea8bc504e65e1bf3bf84b97d726c2db4
HatcheROfficials <hatcher.officials@gmail.com>
2023-02-21 11:25:14 +0530

    Added Planet Shadow. Added Sun Image. Improved planet revolution

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function init() {
 
 const sunR = height / 50; // Radius of sun is the unit all length will be defined
 
+// class defining planets
 class planet {
     radius; // planet radius in terms of suns radius
     orbit; // length of orbit in terms of suns radius
@@ -33,11 +34,18 @@ class planet {
         ctx.fill();
     }
 
+    shadow(){
+        ctx.fillStyle = "rgba(0,0,0,0.5)";
+        ctx.fillRect(this.orbit,0,this.radius*4,this.radius);
+        ctx.fillRect(this.orbit,0,this.radius*4,-this.radius);
+    }
+
     move() {
         ctx.translate(this.orbit, 0);
     }
 }
 
+// class defining each planet moons
 class moon {
     radius; // planet radius in terms of suns radius
     orbit; // length of orbit in terms of suns radius
@@ -71,7 +79,7 @@ var earth = new planet(sunR / 3, sunR * 3, 5, "blue");
 planets.push(earth);
 
 // earth moon
-var earth_moon = new moon(sunR/10, sunR/1.5, 6, "grey");
+var earth_moon = new moon(sunR / 10, sunR / 1.5, 6, "grey");
 earth.moons.push(earth_moon);
 
 // mars
@@ -79,11 +87,12 @@ var mars = new planet(sunR / 3.5, sunR * 4, 8, "red");
 planets.push(mars);
 
 // mars moons
-var mars_moon1 = new moon(sunR/10, sunR/2.2, 12, "grey");
+var mars_moon1 = new moon(sunR / 10, sunR / 2.2, 12, "grey");
 mars.moons.push(mars_moon1);
 
-var mars_moon2 = new moon(sunR/10, sunR/1.8, 6, "grey");
+var mars_moon2 = new moon(sunR / 10, sunR / 1.8, 6, "grey");
 mars.moons.push(mars_moon2);
+
 
 // jupiter
 var jupiter = new planet(sunR / 1.5, sunR * 8, 3, "brown");
@@ -93,10 +102,10 @@ planets.push(jupiter);
 var jupiter_moon1 = new moon(sunR / 7, sunR, 5, "grey");
 jupiter.moons.push(jupiter_moon1);
 
-var jupiter_moon2 = new moon(sunR / 8, sunR*1.2, 4.5, "grey");
+var jupiter_moon2 = new moon(sunR / 8, sunR * 1.2, 4.5, "grey");
 jupiter.moons.push(jupiter_moon2);
 
-var jupiter_moon3 = new moon(sunR / 5, sunR*1.5, 2, "grey");
+var jupiter_moon3 = new moon(sunR / 5, sunR * 1.5, 2, "grey");
 jupiter.moons.push(jupiter_moon3);
 
 // saturn
@@ -104,10 +113,10 @@ var saturn = new planet(sunR / 1.8, sunR * 12, 4, "rgb(234,214,184)");
 planets.push(saturn);
 
 // saturn moons
-var saturn_moon1 = new moon(sunR / 5, sunR*1.5, 2, "grey");
+var saturn_moon1 = new moon(sunR / 5, sunR * 1.5, 2, "grey");
 saturn.moons.push(saturn_moon1);
 
-var saturn_moon2 = new moon(sunR / 5, sunR*2.5, 5, "grey");
+var saturn_moon2 = new moon(sunR / 5, sunR * 2.5, 5, "grey");
 saturn.moons.push(saturn_moon2);
 
 // uranus
@@ -115,13 +124,13 @@ var uranus = new planet(sunR / 2, sunR * 18, 4.5, "rgb(200,150,184)");
 planets.push(uranus);
 
 // uranus moons
-var uranus_moon1 = new moon(sunR / 8, sunR*0.8, 15, "grey");
+var uranus_moon1 = new moon(sunR / 8, sunR * 0.8, 15, "grey");
 uranus.moons.push(uranus_moon1);
 
-var uranus_moon2 = new moon(sunR / 4.5, sunR*1.5, 7, "grey");
+var uranus_moon2 = new moon(sunR / 4.5, sunR * 1.5, 7, "grey");
 uranus.moons.push(uranus_moon2);
 
-var uranus_moon3 = new moon(sunR / 6, sunR*1.2, 3, "grey");
+var uranus_moon3 = new moon(sunR / 6, sunR * 1.2, 3, "grey");
 uranus.moons.push(uranus_moon3);
 
 // neptune
@@ -129,8 +138,9 @@ var neptune = new planet(sunR / 2.5, sunR * 23, 2, "rgb(230,240,150)");
 planets.push(neptune);
 
 // neptune moons
-var neptune_moon = new moon(sunR / 6, sunR*1.2, 3, "grey");
+var neptune_moon = new moon(sunR / 6, sunR * 1.2, 3, "grey");
 neptune.moons.push(neptune_moon);
+
 
 function draw() {
     const time = new Date();
@@ -142,22 +152,30 @@ function draw() {
     ctx.translate(width / 2, height / 2); // bringing origin to center
 
     // sun
-    ctx.fillStyle = "orange";
+    var image = new Image();
+    image.src = "assets/sun.png";
+    ctx.fillStyle = "rgba(255,165,0,0.2";
     ctx.beginPath();
-    ctx.arc(0, 0, sunR, 0, 2 * Math.PI);
+    ctx.arc(0, 0, 2*sunR, 0, 2 * Math.PI);
     ctx.fill();
+    ctx.drawImage(image,-sunR*3/2,-sunR*3/2,sunR*3,sunR*3);
 
     for (p of planets) {
         // drawing the planets
         ctx.save();
-        ctx.rotate(((2 * Math.PI) / (60 / p.angularVel_sun)) * time.getSeconds() +
+        ctx.rotate(((2 * Math.PI) / (1 / p.angularVel_sun)) * time.getMinutes() +
+            ((2 * Math.PI) / (60 / p.angularVel_sun)) * time.getSeconds() +
             ((2 * Math.PI) / (60000 / p.angularVel_sun)) * time.getMilliseconds());
+            
+        p.shadow();
         p.move();
         p.draw();
 
+        // drawing moons
         for (m of p.moons) {
             ctx.save();
-            ctx.rotate(((2 * Math.PI) / (60 / m.angularVel_planet)) * time.getSeconds() +
+            ctx.rotate(((2 * Math.PI) / (1 / m.angularVel_planet)) * time.getMinutes() +
+                ((2 * Math.PI) / (60 / m.angularVel_planet)) * time.getSeconds() +
                 ((2 * Math.PI) / (60000 / m.angularVel_planet)) * time.getMilliseconds());
             m.move();
             m.draw();
@@ -167,14 +185,13 @@ function draw() {
         ctx.restore();
 
         // drawing the orbits
-        ctx.strokeStyle = "rgba(255,0,0,0.4)";
+        ctx.strokeStyle = "rgba(255,255,255,0.4)";
         ctx.beginPath();
         ctx.arc(0, 0, p.orbit, 0, 2 * Math.PI);
         ctx.stroke();
     }
 
     ctx.restore();
-
     window.requestAnimationFrame(draw);
 }
 
